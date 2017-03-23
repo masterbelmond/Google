7679cf489ded9f30ea328a5741e1532aaa9d3173
eliseobeltran <eliseobeltran@gmail.com>
2017-03-23 16:31:02 -0700

    SANDBOX <initial_state> 2017-03-23
    post Suseela's code

--- CL/UPY_ItemFulfillment_Client.js
+++ CL/UPY_ItemFulfillment_Client.js
@@ -1,107 +1,219 @@
-function clientPageInit(a) {}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* @ FILENAME       : UPY_ItemFulfillment_Client.js
+* @ AUTHOR         : eliseo@upayasolutions.com
+* @ DATE           :
+* @ DESCRIPTION    :
+*
+* Copyright (c) 2012 Upaya - The Solution Inc.
+* 10530 N. Portal Avenue, Cupertino CA 95014
+* All Rights Reserved.
+*
+* This software is the confidential and proprietary information of
+* Upaya - The Solution Inc. ("Confidential Information"). You shall not
+* disclose such Confidential Information and shall use it only in
+* accordance with the terms of the license agreement you entered into
+* with Upaya.
+* object
+*/
 
-function clientSaveRecord() {}
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Access mode: create, copy, edit
+ * @returns {Void}
+ */
+function clientPageInit(type){
 
-function clientValidateField(a, b, d) {
-    return !0
 }
 
-function clientFieldChanged(a, b, d) {
-    "item" == a && "T" == nlapiGetCurrentLineItemValue("item", "itemreceive") && (   (b = nlapiGetCurrentLineItemIndex("item"), a = getItemFulfillmentsFields(b), b = getSelectedItemFulfillmentsFields(b), 1 < getCheckedCount() && -1 === a.indexOf(b) && (alert("You cannot group these items"), nlapiSetCurrentLineItemValue("item", "itemreceive", "F")))
-     ||  (b = nlapiGetCurrentLineItemIndex("item"),  c = getSelectedItemHold(b), c && (alert("Line is on Hold"), nlapiSetCurrentLineItemValue("item", "itemreceive", "F"))) 
-	 ||  (b = nlapiGetCurrentLineItemIndex("item"),  qtyAvail = getSelectedItemQty(b), !qtyAvail && (alert("Available Quantity is not enough"), nlapiSetCurrentLineItemValue("item", "itemreceive", "F")))
-	 )
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @returns {Boolean} True to continue save, false to abort save
+ */
+function clientSaveRecord(){
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @param {String} name Field internal id
+ * @param {Number} linenum Optional line item number, starts from 1
+ * @returns {Boolean} True to continue changing field value, false to abort value change
+ */
+function clientValidateField(type, name, linenum){
+
+    return true;
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @param {String} name Field internal id
+ * @param {Number} linenum Optional line item number, starts from 1
+ * @returns {Void}
+ */
+function clientFieldChanged(type, name, linenum)
+{
+	if(type == 'item')
+	{
+		var itemReceive = nlapiGetCurrentLineItemValue('item', 'itemreceive');
+		if(itemReceive == 'T')
+		{
+			var indexItem = nlapiGetCurrentLineItemIndex('item');
+			var itemFulfillmentFields = getItemFulfillmentsFields(indexItem);
+			var selected = getSelectedItemFulfillmentsFields(indexItem);
+			var checkedCount = getCheckedCount();
+
+			if(checkedCount > 1)
+			{
+				//compare
+				var isDuplicate = itemFulfillmentFields.indexOf(selected);
+				if(isDuplicate === -1)
+				{
+					alert('You cannot group these items');
+					nlapiSetCurrentLineItemValue('item', 'itemreceive', 'F');
+				}
+			}
+		}
+	}
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @param {String} name Field internal id
+ * @returns {Void}
+ */
+function clientPostSourcing(type, name) {
+
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @returns {Void}
+ */
+function clientLineInit(type) {
+
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @returns {Boolean} True to save line item, false to abort save
+ */
+function clientValidateLine(type){
+
+    return true;
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @returns {Void}
+ */
+function clientRecalc(type){
+
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @returns {Boolean} True to continue line item insert, false to abort insert
+ */
+function clientValidateInsert(type){
+
+    return true;
+}
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment.
+ * @appliedtorecord recordType
+ *
+ * @param {String} type Sublist internal id
+ * @returns {Boolean} True to continue line item delete, false to abort delete
+ */
+function clientValidateDelete(type){
+
+    return true;
+}
+
+
+var getItemFulfillmentsFields = function(indexItem)
+{
+	var itemFulfillmentLines = [];
+	var itemCount = nlapiGetLineItemCount('item');
+
+	for(var i = 1; i <= itemCount; i++)
+	{
+		var isChecked = nlapiGetLineItemValue('item', 'itemreceive', i);
+		if(isChecked == 'T' && i != indexItem)
+		{
+			var customLineLocation = nlapiGetLineItemValue('item', 'custcol_custom_line_location', i);
+			var requestedDeliveryDate = nlapiGetLineItemValue('item', 'custcol__requested_delivery_date', i);
+			var strRequestedDeliveryDate = nlapiDateToString(nlapiStringToDate(requestedDeliveryDate));
+			var lineShipMethod = nlapiGetLineItemValue('item', 'custcol_line_ship_method', i);
+			var releaseByDate = nlapiGetLineItemValue('item', 'custcol_release_by_date', i);
+			var strReleaseByDate = nlapiDateToString(nlapiStringToDate(releaseByDate));
+		}
+
+		itemFulfillmentLines.push(customLineLocation + '|' + strRequestedDeliveryDate + '|' + lineShipMethod + '|' + strReleaseByDate);
 	}
 
-function clientPostSourcing(a, b) {}
-
-function clientLineInit(a) {}
-
-function clientValidateLine(a) {
-    return !0
+	return itemFulfillmentLines;
 }
 
-function clientRecalc(a) {}
+var getSelectedItemFulfillmentsFields = function(index)
+{
+	var isChecked = nlapiGetLineItemValue('item', 'itemreceive', index);
+	if(isChecked == 'T')
+	{
+		var customLineLocation = nlapiGetLineItemValue('item', 'custcol_custom_line_location', index);
+		var requestedDeliveryDate = nlapiGetLineItemValue('item', 'custcol__requested_delivery_date', index);
+		
+		var strRequestedDeliveryDate = nlapiDateToString(nlapiStringToDate(requestedDeliveryDate));
+		
+		var lineShipMethod = nlapiGetLineItemValue('item', 'custcol_line_ship_method', index);
+		var releaseByDate = nlapiGetLineItemValue('item', 'custcol_release_by_date', index);
+		
+		var strReleaseByDate = nlapiDateToString(nlapiStringToDate(releaseByDate));
+	}
 
-function clientValidateInsert(a) {
-    return !0
+	var itemFulfillmentLines = customLineLocation + '|' + strRequestedDeliveryDate + '|' + lineShipMethod + '|' + strReleaseByDate;
+	return itemFulfillmentLines;
 }
 
-function clientValidateDelete(a) {
-    return !0
-}
-var getItemFulfillmentsFields = function(a) {
-        for (var b = [], d = nlapiGetLineItemCount("item"), c = 1; c <= d; c++) {
-            if ("T" == nlapiGetLineItemValue("item", "itemreceive", c) && c != a) var e = nlapiGetLineItemValue("item", "custcol_custom_line_location", c),
-                f = nlapiGetLineItemValue("item", "custcol__requested_delivery_date", c),
-                g = nlapiGetLineItemValue("item", "custcol_line_ship_method", c),
-                h = nlapiGetLineItemValue("item", "custcol_release_by_date", c);
-            b.push(e + "|" + f + "|" + g + "|" + h)
-        }
-        return b
-    },
-    getSelectedItemFulfillmentsFields = function(a) {
-        if ("T" ==
-            nlapiGetLineItemValue("item", "itemreceive", a)) var b = nlapiGetLineItemValue("item", "custcol_custom_line_location", a),
-            d = nlapiGetLineItemValue("item", "custcol__requested_delivery_date", a),
-            c = nlapiGetLineItemValue("item", "custcol_line_ship_method", a),
-            e = nlapiGetLineItemValue("item", "custcol_release_by_date", a);
-        return b + "|" + d + "|" + c + "|" + e
-    },
-	getSelectedItemHold = function(a) {
-        if ("T" ==
-            nlapiGetLineItemValue("item", "itemreceive", a)) var b = nlapiGetLineItemValue("item", "custcol_line_hold", a);
-		if (b)	
-        return true;
-	    else
-		return false;	
-    },
-	getItemAvailableQty = function(loc,item) {
-		  //alert ("getItemAvailableQty" + loc + "-"+item);
-		  var searchResults = nlapiSearchRecord("item",'customsearch_item_loc_qty_available',
-			[
-			   ["internalid","anyOf",item],
-			   "AND",
-			   ["inventorylocation","anyOf",loc]
-			],
-			[
-			   new nlobjSearchColumn("locationquantityavailable",null,null)
-			]
-		);
-		  if (searchResults != null && searchResults != '')
+var getCheckedCount = function()
+{
+	var itemFulfillmentLines = [];
+	var itemCount = nlapiGetLineItemCount('item');
+	var checkedCount = 0;
+
+	for(var i = 1; i <= itemCount; i++)
+	{
+		var itemReceive = nlapiGetLineItemValue('item', 'itemreceive', i);
+		if(itemReceive == 'T')
 		{
-			//alert ("qty"+ searchResults.length+loc+item);
-			for(var j =0; j < searchResults.length; j++)
-			{
-				var salesOrderValue = searchResults[j];
-				var qty = salesOrderValue.getValue("locationquantityavailable");
-				//alert ("qty"+ qty);
-				return qty;
-			}
-		}	
-		  return 0;	  
-   },
-	getSelectedItemQty = function(a) {
-	  
-	  if ("T" ==
-            nlapiGetLineItemValue("item", "itemreceive", a)) var loc = nlapiGetLineItemValue("item", "custcol_custom_line_location", a),
-            itm = nlapiGetLineItemValue("item", "item", a),
-			itemQty = nlapiGetLineItemValue("item", "quantity", a),
-			remQty = nlapiGetLineItemValue("item", "quantityremaining", a),
-			aQty = getItemAvailableQty(loc,itm);
-			//alert("itemQty" + itemQty +  "   -- " + aQty);
-	  if (itemQty && aQty && parseFloat(itemQty)<=parseFloat(aQty) ) 
-	  {
-		  //alert("itemQty" + itemQty +  "   -- " + aQty);
-		  return true;
-	  } 
- 	  else 
-	  {
-		 return false;	   
-	  } 
-		  
-   },
-    getCheckedCount = function() {
-        for (var a = nlapiGetLineItemCount("item"), b = 0, d = 1; d <= a; d++) "T" == nlapiGetLineItemValue("item", "itemreceive", d) && b++;
-        return b
-    };
+			checkedCount++;
+		}
+	}
+
+	return checkedCount;
+}
